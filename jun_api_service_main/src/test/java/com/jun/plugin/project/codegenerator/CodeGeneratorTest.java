68466b7161c8449705bfbd6e90477aad5aeb9c94
wujun <wujun728@163.com>
2023-07-10 09:44:21 +0800

    1

--- jun_api_service_main/src/test/java/com/jun/plugin/project/codegenerator/CodeGeneratorTest.java
+++ jun_api_service_main/src/test/java/com/jun/plugin/project/codegenerator/CodeGeneratorTest.java
@@ -26,95 +26,24 @@ public class CodeGeneratorTest {
 
     private static final Logger logger = LoggerFactory.getLogger(CodeGeneratorTest.class);
 
-    public static Properties props = new Properties(); // 配置文件
-
     @Test
     public void codeGenerator() throws Exception {
-        GenUtils.PROJECT_PATH = FileUtil.getParent(CodeGeneratorTest.class.getProtectionDomain().getCodeSource().getLocation().getPath(),2);
-        InputStream is = GenUtils.class.getClassLoader().getResourceAsStream("config.properties");
-        props.load(is);
-        GenUtils.TEMPLATE_FILE_PATH = GenUtils.PROJECT_PATH + props.getProperty("templateFilePath");// 模板位置
-
-        DruidDataSource ds = new DruidDataSource();
-        ds.setDriverClassName(GenUtils.props.getProperty("driver"));
-        ds.setUrl(GenUtils.props.getProperty("url"));
-        ds.setUsername(GenUtils.props.getProperty("uname"));
-        ds.setPassword(GenUtils.props.getProperty("pwd"));
-        List<String> tableNames = Arrays.asList("sys_user", "abc");
-        List<ClassInfo> classInfos = Lists.newArrayList();
-        tableNames.stream().forEach(t -> {
-            Table table = MetaUtil.getTableMeta(ds, t);
-            if(table.getPkNames().size()>0){//没有主键是不生成的
-                classInfos.add(GenUtils.getClassInfo(table));
-            }
-        });
-        genTables(classInfos);
-    }
-
-
-    public static void genTables(List<ClassInfo> classInfos) throws Exception {
-        genTables(classInfos, "");
-    }
-
-    public static void genTables(List<ClassInfo> classInfos, String genType) throws Exception {
-        if (StringUtils.isEmpty(genType)) {
-            genType = "single" ;
-        }
-        List<String> templates = getTemplates();
-//		switch (genType){
-//			case "single" : templates = getTemplates();break;
-////			case "parent" : templates = getTemplates();break;
-//		}
-//        List<ClassInfo> classInfos = GenUtils.getClassInfo(tables);
-        classInfos.forEach(classInfo -> {
-            Map<String, Object> datas = new HashMap<String, Object>();
-            datas.put("classInfo", classInfo);
-//            datas.put("authorName", "wujun");
-//            datas.put("isLombok", true);
-//            datas.put("isAutoImport", true);
-//            datas.put("isWithPackage", true);
-//            datas.put("isSwagger", true);
-//            datas.put("isComment", true);
-//            datas.put("packageName", GenUtils.PACKAGE);
-            PropsUtil.get("config").forEach((k,v)->{
-                if(String.valueOf(v).equalsIgnoreCase("true")||String.valueOf(v).equalsIgnoreCase("false")){
-                    datas.put(String.valueOf(k), Boolean.valueOf(String.valueOf(v)));
-                }else{
-                    datas.put(String.valueOf(k), v);
-                }
-            });
-            Map<String, String> result = new HashMap<String, String>();
-            try {
-                result = GenUtils.processTemplatesStringWriter(datas, templates);
-                GenUtils.processTemplatesFileWriter(classInfo, datas, templates);
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (TemplateException e) {
-                e.printStackTrace();
-            }
-            // 计算,生成代码行数
-            int lineNum = 0;
-            for (Map.Entry<String, String> item : result.entrySet()) {
-                if (item.getValue() != null) {
-                    lineNum += StringUtils.countMatches(item.getValue(), "\n");
-                }
-            }
-            logger.info("生成代码行数：{}", lineNum);
-        });
-        if (CollectionUtils.isEmpty(classInfos)) {
-            logger.error("找不到当前表的元数据classInfos.size()：{}", classInfos.size());
-        }
+        GenUtils.init(PropsUtil.get("config").toProperties(),getTemplates());
+        GenUtils.genCode(Arrays.asList("sys_user", "abc"));
     }
 
     public static List<String> getTemplates() {
         List<String> templates = Lists.newArrayList();
-        templates.add("code-generator/mybatis-plus-single/controller.ftl");
-        templates.add("code-generator/mybatis-plus-single/entity.ftl");
-        templates.add("code-generator/mybatis-plus-single/mapper.ftl");
-        templates.add("code-generator/mybatis-plus-single/service.ftl");
-        templates.add("code-generator/mybatis-plus-single/dto.ftl");
-        templates.add("code-generator/mybatis-plus-single/vo.ftl");
-        templates.add("code-generator/mybatis-plus-single/service.impl.ftl");
+        templates.add("code-generator/mybatis-plus-single/controller.java.ftl");
+        templates.add("code-generator/mybatis-plus-single/dto.java.ftl");
+        templates.add("code-generator/mybatis-plus-single/entity.java.ftl");
+        templates.add("code-generator/mybatis-plus-single/mapper.java.ftl");
+        templates.add("code-generator/mybatis-plus-single/service.java.ftl");
+        templates.add("code-generator/mybatis-plus-single/service.impl.java.ftl");
+        templates.add("code-generator/mybatis-plus-single/vo.java.ftl");
+        templates.add("code-generator/mybatis-plus-single/menu.sql.ftl");
+        templates.add("code-generator/mybatis-plus-single/edit.html.ftl");
+        templates.add("code-generator/mybatis-plus-single/list.html.ftl");
         return templates;
     }
 
